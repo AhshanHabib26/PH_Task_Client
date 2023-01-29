02c847ac162d7afec3f161c75eb13ae6a1972a51
AhshanHabib26 <ahshan.habib026@gmail.com>
2023-01-30 03:24:54 +0600

    Dashboard Added

---
+++ src/Pages/Dashboard/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className=" flex justify-between mx-8 py-3 shadow-md  items-center ">
+      <h2 className=' text-white text-lg'>Power Hack</h2>
+      <h2 className=' text-white text-lg'>Paid Total: 0</h2>
+    </div>
+  );
+};
+
+export default Navbar;
